85a49c875ccef5604ced9b358c97c8dd7a8971af
Quphoria <s.simpson41.ss@googlemail.com>
2022-05-29 23:44:21 +0100

    Update version tag

--- backend/build.bat
+++ backend/build.bat
@@ -1,6 +1,6 @@
 REM Change directory to script directory
 cd /D "%~dp0"
 
-set tag="quphoria/hiqnet-websocket-proxy"
+set tag="quphoria/hiqnet-websocket-proxy:v1.0.0"
 
 docker build --tag %tag% .
--- website/build.bat
+++ website/build.bat
@@ -1,6 +1,6 @@
 REM Change directory to script directory
 cd /D "%~dp0"
 
-set tag="quphoria/soundweb-control"
+set tag="quphoria/soundweb-control:v1.0.0"
 
 docker build --tag %tag% .
