0931af077c961d640a2b269be598065bb49e16b3
Quphoria <s.simpson41.ss@googlemail.com>
2022-06-02 15:30:51 +0100

    Update build scripts and install extra deps on arm

--- backend/build.bat
+++ backend/build.bat
@@ -1,9 +1,13 @@
 REM Change directory to script directory
 cd /D "%~dp0"
 
-set tag="quphoria/hiqnet-websocket-proxy:v1.0.1"
+SET latest=1
+
+SET tag="quphoria/hiqnet-websocket-proxy:v1.0.1"
+SET latesttag=
+if defined latest SET latesttag=--tag "quphoria/hiqnet-websocket-proxy"
 
 @REM docker build --tag %tag% .
 docker buildx create --name soundwebbuilder --use
-docker buildx build --platform=linux/amd64,linux/arm64 --tag %tag% .
+docker buildx build --platform=linux/amd64,linux/arm64 --push --tag %tag% %latesttag% .
 docker buildx rm soundwebbuilder
