83e2137157bfc07136b778f942f14677289582f9
Quphoria <s.simpson41.ss@googlemail.com>
2022-06-02 14:48:22 +0100

    Fix build commands

--- backend/build.bat
+++ backend/build.bat
@@ -4,4 +4,4 @@ cd /D "%~dp0"
 set tag="quphoria/hiqnet-websocket-proxy:v1.0.1"
 
 @REM docker build --tag %tag% .
-docker buildx --platform=linux/amd64,linux/arm64 --tag %tag% .
+docker buildx build --platform=linux/amd64,linux/arm64 --tag %tag% .
--- website/build.bat
+++ website/build.bat
@@ -4,4 +4,4 @@ cd /D "%~dp0"
 set tag="quphoria/soundweb-control:v1.0.1"
 
 @REM docker build --tag %tag% .
-docker buildx --platform=linux/amd64,linux/arm64 --tag %tag% .
+docker buildx build --platform=linux/amd64,linux/arm64 --tag %tag% .
