edc068fd38970c92d009cb2c22ae2aa544bc8357
Quphoria <s.simpson41.ss@googlemail.com>
2022-06-02 14:53:50 +0100

    Update build commands

--- backend/build.bat
+++ backend/build.bat
@@ -4,4 +4,6 @@ cd /D "%~dp0"
 set tag="quphoria/hiqnet-websocket-proxy:v1.0.1"
 
 @REM docker build --tag %tag% .
+docker buildx create --name soundwebbuilder --use
 docker buildx build --platform=linux/amd64,linux/arm64 --tag %tag% .
+docker buildx rm soundwebbuilder
--- website/build.bat
+++ website/build.bat
@@ -4,4 +4,6 @@ cd /D "%~dp0"
 set tag="quphoria/soundweb-control:v1.0.1"
 
 @REM docker build --tag %tag% .
+docker buildx create --name soundwebbuilder --use
 docker buildx build --platform=linux/amd64,linux/arm64 --tag %tag% .
+docker buildx rm soundwebbuilder
