72bdbe3e8f63cd48715188f68ebc5282fc7afd41
Quphoria <s.simpson41.ss@googlemail.com>
2022-06-21 14:39:16 +0100

    Change backend websocket server for performance

--- backend/websocket_bridge_multi.py
+++ backend/websocket_bridge_multi.py
@@ -1,8 +1,9 @@
-import asyncio, websockets, os
+import asyncio, os, threading, zlib
 from janus import Queue
 import json, hmac, hashlib, time
 from config import load_config
 from concurrent.futures import ThreadPoolExecutor
+from websocket_server import WebsocketServer
 
 from soundweb_proto import MessageType, Packet, meter_value_db, decode_packets, DecodeFailed
 from soundweb_client import SoundWebThread
@@ -11,38 +12,27 @@ token_time_range = 10 * 60 * 1000 # +-10 minutes
 
 subscribed_params = {}
 
+ws_server = None
+WS_DATA_LOCK = threading.Lock()
 WEBSOCKET_LIST = []
+WS_USER_DATA = {}
 
+param_cache_lock = threading.Lock()
 param_cache = {}
 
 client_thread_status = {}
 
-async def resp_broadcast_send(websocket, data: str):
-    try:
-        await websocket.send(data)
-    except websockets.exceptions.ConnectionClosedError:
-        if websocket in WEBSOCKET_LIST:
-            WEBSOCKET_LIST.remove(websocket)
-    except Exception as ex:
-        print("Broadcast Error: " + str(ex), flush=True)
-        try:
-            await websocket.close()
-        except:
-            pass
-        if websocket in WEBSOCKET_LIST:
-            WEBSOCKET_LIST.remove(websocket)
-
 async def resp_broadcast(node: str):
-    global param_cache
+    global param_cache, param_cache_lock, ws_server, WEBSOCKET_LIST
     while True:
         # Get a "work item" out of the queue.
         msg = await resp_queues[node].async_q.get()
         if msg["type"] == "SET":
-            param_cache[msg["parameter"]] = json.dumps(msg)
+            with param_cache_lock:
+                param_cache[msg["parameter"]] = json.dumps(msg)
         data = json.dumps(msg)
-        # send all at the same time instead of doing sequentially
         if WEBSOCKET_LIST:
-            await asyncio.wait([asyncio.create_task(resp_broadcast_send(websocket, data)) for websocket in WEBSOCKET_LIST])
+            ws_server.send_message_to_list(WEBSOCKET_LIST, data)
 
 def get_packet_node_handler(p: Packet) -> str:
     global config
@@ -81,89 +71,83 @@ def check_auth_token_hmac(message: str):
         pass
     return False, None
 
-process_pool = ThreadPoolExecutor(2)
+def ws_on_connection_open(client, server):
+    print("Websocket Connection:", client["address"], flush=True)
 
-def websocket_send(websocket, message):
-    # this uses websocket.write_frame_sync internally and should give better performance
-    websockets.broadcast([websocket], message)
+def close_ws_client(client, server):
+    client["handler"].send_close(1000, b'')
+    server._terminate_client_handler(client["handler"])
 
-async def msg_handler(websocket):
-    print("Websocket Connection:", websocket.remote_address, flush=True)
-    user_data = None
-    user_options = {}
-    try:
-        first_message = True
-        async for message in websocket:
-            # print("WS MSG:", message, flush=True)
-            if first_message:
-                # break out of loop if auth token invalid
-                # run check_auth_token_hmac in executor to prevent hanging the main thread with hmac calculations
-                loop = asyncio.get_event_loop()
-                auth_valid, user_data = await loop.run_in_executor(process_pool, check_auth_token_hmac, message)
-                if user_data is None or not auth_valid:
-                    await websocket.close()
-                    break
-                user_options = user_data.get("options", {})
-                # send __test__ to acknowledge websocket auth
-                await websocket.send("__test__")
-                if not user_options.get("status", False):
-                    for p in param_cache.values():
-                        await websocket.send(p)
-                    WEBSOCKET_LIST.append(websocket)
-                first_message = False
-            elif message == "__test__":
-                websocket_send(websocket, "__test__")
-            elif message == "status":
-                if user_data.get("admin", False):
-                    websocket_send(websocket, json.dumps({
-                        "type": "status",
-                        "data": client_thread_status
-                    }))
-            elif message == "version":
-                if user_data.get("admin", False):
-                    websocket_send(websocket, json.dumps({
-                        "type": "version",
-                        "data": os.environ.get('VERSION', "Unknown")
-                    }))
-            elif not user_options.get("statusonly", False):
-                try:
-                    p = Packet.from_json(json.loads(message))
-                    sub_handler_node = get_packet_node_handler(p)
-                    # print(p, flush=True)
-                    sent_value = False
-                    if p.message_type == MessageType.SUBSCRIBE or p.message_type == MessageType.SUBSCRIBE_PERCENT:
-                        p.value = config["subscription_rate_ms"]
-                        for sub in subscribed_params[sub_handler_node]:
-                            if sub.param_str() == p.param_str() and sub.message_type == p.message_type:
-                                if p.param_str() in param_cache: # avoid resubscribing to parameters if value cached
-                                    websocket_send(websocket, param_cache[p.param_str()])
-                                    sent_value = True
-                                else:
-                                    subscribed_params[sub_handler_node].remove(sub)
-                        if not sent_value: # avoid resubscribing to parameters
-                            subscribed_params[sub_handler_node].append(p)
-                            await subscribe_queues[sub_handler_node].async_q.put(p)
-                    elif p.message_type == MessageType.UNSUBSCRIBE or p.message_type == MessageType.UNSUBSCRIBE_PERCENT:
-                        for sub in subscribed_params[sub_handler_node]:
-                            if sub.param_str() == p.param_str() and sub.message_type == p.message_type:
-                                subscribed_params[sub_handler_node].remove(sub)
-                        await subscribe_queues[sub_handler_node].async_q.put(p)
-                    else:
-                        await msg_queue.async_q.put(p)
-                except (json.JSONDecodeError, DecodeFailed) as ex:
-                    print("Failed to decode:", ex, ":", message, flush=True)
-    except websockets.exceptions.ConnectionClosedError:
-        pass
-    except Exception as ex:
-        if websocket in WEBSOCKET_LIST:
-            WEBSOCKET_LIST.remove(websocket)
-        raise ex
-    try:
-        await websocket.close()
-    except:
-        pass
-    if websocket in WEBSOCKET_LIST:
-        WEBSOCKET_LIST.remove(websocket)
+def ws_on_data_receive(client, server, message):
+    global WEBSOCKET_LIST, WS_USER_DATA, WS_DATA_LOCK
+    user_data, user_options = WS_USER_DATA.get(client["address"], (None, None))
+    if user_data is None:
+        # close websocket if auth token invalid
+        auth_valid, user_data = check_auth_token_hmac(message)
+        if user_data is None or not auth_valid:
+            close_ws_client(client, server)
+            return
+        user_options = user_data.get("options", {})
+        with WS_DATA_LOCK:
+            WS_USER_DATA[client["address"]] = (user_data, user_options)
+            if not user_options.get("status", False):
+                WEBSOCKET_LIST.append(client)
+        # send __test__ to acknowledge websocket auth
+        server.send_message(client, "__test__")
+        # if not user_options.get("status", False):
+        #     # we don't need to send the entire param_cache as the values will get sent when the client subscribes to them
+        #     for p in param_cache.values():
+        #         server.send(client, p)
+    elif message == "__test__":
+        server.send_message(client, "__test__")
+    elif message == "status":
+        if user_data.get("admin", False):
+            server.send_message(client, json.dumps({
+                "type": "status",
+                "data": client_thread_status
+            }))
+    elif message == "version":
+        if user_data.get("admin", False):
+            server.send_message(client, json.dumps({
+                "type": "version",
+                "data": os.environ.get('VERSION', "Unknown")
+            }))
+    elif not user_options.get("statusonly", False):
+        try:
+            p = Packet.from_json(json.loads(message))
+            sub_handler_node = get_packet_node_handler(p)
+            # print(p, flush=True)
+            sent_value = False
+            if p.message_type == MessageType.SUBSCRIBE or p.message_type == MessageType.SUBSCRIBE_PERCENT:
+                p.value = config["subscription_rate_ms"]
+                for sub in subscribed_params[sub_handler_node]:
+                    if sub.param_str() == p.param_str() and sub.message_type == p.message_type:
+                        if p.param_str() in param_cache: # avoid resubscribing to parameters if value cached
+                            server.send_message(client, param_cache[p.param_str()])
+                            sent_value = True
+                        else:
+                            subscribed_params[sub_handler_node].remove(sub)
+                if not sent_value: # avoid resubscribing to parameters
+                    subscribed_params[sub_handler_node].append(p)
+                    subscribe_queues[sub_handler_node].sync_q.put(p)
+            elif p.message_type == MessageType.UNSUBSCRIBE or p.message_type == MessageType.UNSUBSCRIBE_PERCENT:
+                for sub in subscribed_params[sub_handler_node]:
+                    if sub.param_str() == p.param_str() and sub.message_type == p.message_type:
+                        subscribed_params[sub_handler_node].remove(sub)
+                subscribe_queues[sub_handler_node].sync_q.put(p)
+            else:
+                msg_queue.sync_q.put(p)
+        except (json.JSONDecodeError, DecodeFailed) as ex:
+            print("Failed to decode:", ex, ":", message, flush=True)
+
+def ws_on_connection_close(client, server):
+    global WS_DATA_LOCK, WEBSOCKET_LIST, WS_USER_DATA
+    with WS_DATA_LOCK:
+        if client in WEBSOCKET_LIST:
+            WEBSOCKET_LIST.remove(client)
+        ip = client["address"]
+        if ip in WS_USER_DATA:
+            WS_USER_DATA.pop(ip)
 
 health_check_queue = None
 
@@ -186,11 +170,11 @@ async def health_check():
 
         healthy = all(s for s in client_thread_status.values())
         update_health(healthy)
-    
+
 soundweb_thread = None
 soundweb_subscribe_threads = []
 async def main():
-    global config, msg_queue, resp_queues, subscribe_queues, soundweb_thread, soundweb_subscribe_threads, subscribed_params, health_check_queue
+    global config, ws_server, msg_queue, resp_queues, subscribe_queues, soundweb_thread, soundweb_subscribe_threads, subscribed_params, health_check_queue
     msg_queue = Queue(200)
     health_check_queue = Queue(50)
     resp_queues = {key: Queue(200) for key in config["nodes"].keys()}
@@ -206,11 +190,15 @@ async def main():
     for t in soundweb_subscribe_threads.values():
         t.start()
     print(f"Websocket server listening on ws://0.0.0.0:{config['websocket_port']}", flush=True)
-    async with websockets.serve(msg_handler, "0.0.0.0", config["websocket_port"]):
-        for node in config["nodes"]:
-            asyncio.create_task(resp_broadcast(node))
-        asyncio.create_task(health_check())
-        await asyncio.Future()  # run forever
+    
+    for node in config["nodes"]:
+        asyncio.create_task(resp_broadcast(node))
+    ws_server = WebsocketServer(host="0.0.0.0", port=config["websocket_port"])
+    ws_server.set_fn_new_client(ws_on_connection_open)
+    ws_server.set_fn_client_left(ws_on_connection_close)
+    ws_server.set_fn_message_received(ws_on_data_receive)
+    ws_server.run_forever(threaded=True)
+    await health_check()
 
 if __name__ == "__main__":
     update_health(False) # set healthy to false when starting
@@ -220,10 +208,14 @@ if __name__ == "__main__":
         asyncio.run(main())
     except KeyboardInterrupt:
         pass
+    print("Exiting...")
 
     if soundweb_thread:
         soundweb_thread.exitFlag = True
         soundweb_thread.join()
+    if ws_server:
+        ws_server.shutdown_gracefully()
+        # should figure out how to get thread to exit, but thread is daemonized so will get killed when program exits
     for t in soundweb_subscribe_threads.values():
         t.exitFlag = True
         t.join()
--- website/build.bat
+++ website/build.bat
@@ -2,7 +2,7 @@ REM Change directory to script directory
 cd /D "%~dp0"
 
 SET BASETAG=quphoria/soundweb-control
-SET VERSION=v1.0.3
+SET VERSION=v1.0.4
 SET latest=1
 
 SET tag="%BASETAG%:%VERSION%"
