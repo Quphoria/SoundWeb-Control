6f16b557d2d7c373b5120c1182cb3886bb45966c
Quphoria <s.simpson41.ss@googlemail.com>
2022-11-12 02:42:05 +0000

    Fix font CSS

--- website/soundweb-control/pages/_app.js
+++ website/soundweb-control/pages/_app.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import '../App.css';
 
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
--- website/soundweb-control/pages/_document.js
+++ website/soundweb-control/pages/_document.js
@@ -5,17 +5,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <style>
-            @font-face {
-              font-family: "Microsoft Sans Serif";
-              src: url("https://db.onlinewebfonts.com/t/2442df04682466647c9b737e374dd1ef.eot"); /* IE9*/
-              src: url("https://db.onlinewebfonts.com/t/2442df04682466647c9b737e374dd1ef.eot?#iefix") format("embedded-opentype"), /* IE6-IE8 */
-              url("https://db.onlinewebfonts.com/t/2442df04682466647c9b737e374dd1ef.woff2") format("woff2"), /* chrome firefox */
-              url("https://db.onlinewebfonts.com/t/2442df04682466647c9b737e374dd1ef.woff") format("woff"), /* chrome firefox */
-              url("https://db.onlinewebfonts.com/t/2442df04682466647c9b737e374dd1ef.ttf") format("truetype"), /* chrome firefox opera Safari, Android, iOS 4.2+*/
-              url("https://db.onlinewebfonts.com/t/2442df04682466647c9b737e374dd1ef.svg#Microsoft Sans Serif") format("svg"); /* iOS 4.1- */
-            }
-          </style>
         </Head>
         <body>
           <Main />
