7ae8ac42d1e2a56fe05092c250e6f387f3886866
Quphoria <s.simpson41.ss@googlemail.com>
2023-07-27 18:26:07 +0100

    Change config parser to allow config migration

--- website/soundweb-control/config/config.js
+++ website/soundweb-control/config/config.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
-import { jsonc } from 'jsonc';
+const { parse, stringify, assign} = require('comment-json');
 
 const filename = ( process.env.DATA_DIR || "" ) + "config.jsonc";
-const defaultConfig = `{
+const defaultConfigString = `{
   // This must be kept secret and be at least 32 characters long
   // This is used to encrypt session cookies
   // Generate a 100 character password here: https://1password.com/password-generator/
@@ -18,53 +18,89 @@ const defaultConfig = `{
   "allowedSSOOrigins": []
 }`;
 
+var defaultConfig = {};
+try {
+  defaultConfig = parse(defaultConfigString);
+} catch (err) {
+  console.log("Error loading default config:", err);
+  throw "Failed to load default config";
+}
+
 var config = {};
 
 var backup_on_error = true;
 var jsonString = "";
+var invalid_optional_config = false;
 try {
   jsonString = fs.readFileSync(filename, "utf8");
-  config = jsonc.parse(jsonString);
-  if (typeof(config.sessionPassword) !== "string" || config.sessionPassword.length < 32) {
-    throw "sessionPassword must be a secret AND SECURE 32+ character string";
+  config = parse(jsonString);
+
+  const required_config_tests = { // Test returns false on failure (or throws an error)
+    "sessionPassword": (x) => {
+      if (typeof(x) !== "string" || x < 32) throw "sessionPassword must be a secret AND SECURE 32+ character string";
+      if (x === "complex_password_at_least_32_characters_long" && process.env.NODE_ENV === "production") {
+        console.log("\n")
+        console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        console.log("!!! sessionPassword has not been changed from the default value, please edit config.jsonc !!!")
+        console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        backup_on_error = false; // Don't backup on this error as this may loop after overwriting the config, then deleting the backup
+        throw "sessionPassword is insecure"
+      }
+      return true;
+    },
+    "soundwebBridgeWebsocket": (x) => (typeof(x) === "string"),
+    "useSSL": (x) => (typeof(x) === "boolean"),
+    "authTokenSecret": (x) => {
+      if (typeof(x) !== "string" || x < 32) throw "authTokenSecret must be a secret AND SECURE 32+ character string";
+      if (x === "complex_password_at_least_32_characters_long" && process.env.NODE_ENV === "production") {
+        console.log("\n")
+        console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        console.log("!!! authTokenSecret has not been changed from the default value, please edit config.jsonc !!!")
+        console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        backup_on_error = false; // Don't backup on this error as this may loop after overwriting the config, then deleting the backup
+        throw "authTokenSecret is insecure"
+      }
+      return true;
+    },
+  };
+
+  for (const [key, test] of Object.entries(required_config_tests)) {
+    if (test(config[key])) continue;
+
+    throw `Missing ${key}`;
   }
-  if (config.sessionPassword === "complex_password_at_least_32_characters_long" && process.env.NODE_ENV === "production") {
-    console.log("\n")
-    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    console.log("!!! sessionPassword has not been changed from the default value, please edit config.jsonc !!!")
-    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    backup_on_error = false; // Don't backup on this error as this may loop after overwriting the config, then deleting the backup
-    throw "sessionPassword is insecure"
+
+  const optional_config_tests = { // Test returns false on failure (or throws an error)
+    "allowedSSOOrigins": (x) => (Array.isArray(x) && x.find((a) => typeof(a) !== "string") === undefined),
+  };
+
+  for (const [key, test] of Object.entries(optional_config_tests)) {
+    try {
+      if (test(config[key])) continue;
+    } catch (e) {
+      console.log(`Error with config entry ${key} test:`, e);
+    }
+    console.log(`Config entry ${key} is invalid`);
+    invalid_optional_config = true;
+    config = assign(config, defaultConfig, [key]);
   }
-  if (typeof(config.soundwebBridgeWebsocket) !== "string") {
-    throw "Missing soundwebBridgeWebsocket";
-  }
-  if (typeof(config.useSSL) !== "boolean") {
-    throw "Missing useSSL";
-  }
-  if (typeof(config.authTokenSecret) !== "string" || config.authTokenSecret.length < 32) {
-    throw "authTokenSecret must be a secret AND SECURE 32+ character string";
-  }
-  if (config.authTokenSecret === "different_password_at_least_32_characters_long" && process.env.NODE_ENV === "production") {
-    console.log("\n")
-    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    console.log("!!! authTokenSecret has not been changed from the default value, please edit config.jsonc !!!")
-    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    backup_on_error = false;
-    throw "authTokenSecret is insecure"
-  }
-  if (!Array.isArray(config.allowedSSOOrigins) || config.allowedSSOOrigins.find((x) => typeof(x) !== "string") !== undefined) {
-    throw "Missing allowedSSOOrigins";
-  }
+
+  if (invalid_optional_config) throw "Invalid optional config entry"
+
 } catch (err) {
   console.log("\n")
   console.log("Error loading config:", err);
-  fs.writeFileSync(filename, defaultConfig);
   if (backup_on_error && jsonString) {
     console.log("Previous config saved to:", filename + ".backup");
     fs.writeFileSync(filename + ".backup", jsonString);
   }
-  throw "Failed to load config";
+
+  if (invalid_optional_config) {
+    fs.writeFileSync(filename, stringify(config, null, 2));
+  } else {
+    fs.writeFileSync(filename, defaultConfig);
+    throw "Failed to load config";
+  }
 }
 
 export default config;
